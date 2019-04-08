9d90803faf04d2e8ccec53d1210db5344c8bd91a
Jameson Campbell <jcampbell@flexterapp.com>
2019-04-08 15:27:27 -0500

    TA Discussion

--- assets/app.js
+++ assets/app.js
@@ -13,14 +13,35 @@ function renderButtons() {
 
 // This function handles events where one button is clicked
 $("#add-gif-topic").on("click", function (event) {
-
     event.preventDefault();
     var text = $(`#gif-topic-input`).val();
     topics.push(text);
     renderButtons();
-
 });
 
+function makeGif(topic) {
+    for (var i = 0; i <= 10; i++){
+        return  `
+    <div> 
+      <h4>Rating: ${topic.data[i].rating}</h4>
+      <img src='${topic.data[i].images.original_still.url}'/>
+    </div>
+      ` 
+    }
+}
+
+$(document).on('click', '.gif', function () {
+    var topic = $(this).text();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=oxClL7egdUmOnYP5STHkYWt3wkn2I0yX&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        $(`#gifs-view`).append(makeGif(response));
+    })
+})
 
 
 renderButtons();
